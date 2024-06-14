cc15a23801bdc9a06fca95e0097eced0525fff91
Marcos Martins <marcosoliveiraeu@gmail.com>
2024-06-14 18:16:06 -0300

    config prod

--- src/utils/apiConfig.js
+++ src/utils/apiConfig.js
@@ -1,3 +1,5 @@
-const API_URL = 'https://localhost:7137/api';
+//const API_URL = 'https://localhost:7137/api';
+
+const API_URL = 'https://marcosoliveiraeu.bsite.net/api';
 
 export default API_URL;
